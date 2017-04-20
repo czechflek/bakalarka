9a2774d01cae1d999192902049f40758fcbf11b4
Jakub Cech <kuba7593@gmail.com>
2017-04-20 13:24:37 +0200

    Database model update

--- models/create_db.sql
+++ models/create_db.sql
@@ -7,6 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema bachelors
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `bachelors` ;
 
 -- -----------------------------------------------------
 -- Schema bachelors
@@ -20,71 +21,65 @@ USE `bachelors` ;
 DROP TABLE IF EXISTS `bachelors`.`user` ;
 
 CREATE TABLE IF NOT EXISTS `bachelors`.`user` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `userId` VARCHAR(20) NOT NULL,
-  `username` VARCHAR(25) NULL DEFAULT NULL,
+  `id` VARCHAR(40) NOT NULL,
+  `username` VARCHAR(25) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_online` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `active` TINYINT NULL DEFAULT 1,
-  `health` INT NULL DEFAULT 200,
-  `experience` INT NULL DEFAULT 0,
-  `level` INT NULL DEFAULT 1,
-  `gold` INT NULL DEFAULT 0,
-  `gems` INT NULL DEFAULT 0,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_online` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `active` BIT NOT NULL DEFAULT 1,
+  `health` INT NOT NULL DEFAULT 200,
+  `experience` INT NOT NULL DEFAULT 0,
+  `level` INT NOT NULL DEFAULT 1,
+  `gold` INT NOT NULL DEFAULT 0,
+  `gems` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `idString_UNIQUE` (`userId` ASC));
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC));
 
 
 -- -----------------------------------------------------
--- Table `bachelors`.`gameObjectType`
+-- Table `bachelors`.`game_object_type`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bachelors`.`gameObjectType` ;
+DROP TABLE IF EXISTS `bachelors`.`game_object_type` ;
 
-CREATE TABLE IF NOT EXISTS `bachelors`.`gameObjectType` (
+CREATE TABLE IF NOT EXISTS `bachelors`.`game_object_type` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
-  `ownable` TINYINT NULL DEFAULT 0,
+  `ownable` BIT NULL DEFAULT 0,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bachelors`.`gameObject`
+-- Table `bachelors`.`game_object`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bachelors`.`gameObject` ;
+DROP TABLE IF EXISTS `bachelors`.`game_object` ;
 
-CREATE TABLE IF NOT EXISTS `bachelors`.`gameObject` (
+CREATE TABLE IF NOT EXISTS `bachelors`.`game_object` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `parent_id` INT UNSIGNED NOT NULL,
   `count` INT NOT NULL DEFAULT 1,
-  `gameObjectType_id` INT UNSIGNED NOT NULL,
+  `root` BIT NOT NULL DEFAULT 1,
+  `game_object_type_id` INT UNSIGNED NOT NULL,
+  `description` VARCHAR(200) NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_gameObject_gameObject1_idx` (`parent_id` ASC),
-  INDEX `fk_gameObject_gameObjectType1_idx` (`gameObjectType_id` ASC),
-  CONSTRAINT `fk_gameObject_gameObject1`
-    FOREIGN KEY (`parent_id`)
-    REFERENCES `bachelors`.`gameObject` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_gameObject_gameObjectType1`
-    FOREIGN KEY (`gameObjectType_id`)
-    REFERENCES `bachelors`.`gameObjectType` (`id`)
+  INDEX `fk_game_object_game_object_type1_idx` (`game_object_type_id` ASC),
+  CONSTRAINT `fk_game_object_game_object_type1`
+    FOREIGN KEY (`game_object_type_id`)
+    REFERENCES `bachelors`.`game_object_type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bachelors`.`locationType`
+-- Table `bachelors`.`location_type`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bachelors`.`locationType` ;
+DROP TABLE IF EXISTS `bachelors`.`location_type` ;
 
-CREATE TABLE IF NOT EXISTS `bachelors`.`locationType` (
+CREATE TABLE IF NOT EXISTS `bachelors`.`location_type` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
 ENGINE = InnoDB;
 
 
@@ -95,33 +90,33 @@ DROP TABLE IF EXISTS `bachelors`.`location` ;
 
 CREATE TABLE IF NOT EXISTS `bachelors`.`location` (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `lat` DOUBLE NOT NULL,
-  `lon` DOUBLE NOT NULL,
-  `areacode` VARCHAR(10) NOT NULL,
-  `gameObject_id` INT UNSIGNED NOT NULL,
-  `locationType_id` INT UNSIGNED NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `latitude` DOUBLE NOT NULL,
+  `longitude` DOUBLE NOT NULL,
+  `game_object_id` INT UNSIGNED NULL,
+  `location_type_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_location_gameObject1_idx` (`gameObject_id` ASC),
-  INDEX `fk_location_locationType1_idx` (`locationType_id` ASC),
-  CONSTRAINT `fk_location_gameObject1`
-    FOREIGN KEY (`gameObject_id`)
-    REFERENCES `bachelors`.`gameObject` (`id`)
+  INDEX `fk_location_game_object1_idx` (`game_object_id` ASC),
+  INDEX `fk_location_location_type1_idx` (`location_type_id` ASC),
+  CONSTRAINT `fk_location_game_object1`
+    FOREIGN KEY (`game_object_id`)
+    REFERENCES `bachelors`.`game_object` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_location_locationType1`
-    FOREIGN KEY (`locationType_id`)
-    REFERENCES `bachelors`.`locationType` (`id`)
+  CONSTRAINT `fk_location_location_type1`
+    FOREIGN KEY (`location_type_id`)
+    REFERENCES `bachelors`.`location_type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bachelors`.`attributeKey`
+-- Table `bachelors`.`attribute_key`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bachelors`.`attributeKey` ;
+DROP TABLE IF EXISTS `bachelors`.`attribute_key` ;
 
-CREATE TABLE IF NOT EXISTS `bachelors`.`attributeKey` (
+CREATE TABLE IF NOT EXISTS `bachelors`.`attribute_key` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
@@ -129,52 +124,75 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bachelors`.`gameObjectTypeAttribute`
+-- Table `bachelors`.`game_object_type_attribute`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bachelors`.`gameObjectTypeAttribute` ;
+DROP TABLE IF EXISTS `bachelors`.`game_object_type_attribute` ;
 
-CREATE TABLE IF NOT EXISTS `bachelors`.`gameObjectTypeAttribute` (
-  `gameObjectType_id` INT UNSIGNED NOT NULL,
-  `attributeKey_id` INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `bachelors`.`game_object_type_attribute` (
+  `game_object_type_id` INT UNSIGNED NOT NULL,
+  `attribute_key_id` INT UNSIGNED NOT NULL,
   `value` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`gameObjectType_id`, `attributeKey_id`),
-  INDEX `fk_gameObjectDefaultAttribute_gameObjectType1_idx` (`gameObjectType_id` ASC),
-  INDEX `fk_gameObjectTypeAttribute_attributeKey1_idx` (`attributeKey_id` ASC),
-  CONSTRAINT `fk_gameObjectDefaultAttribute_gameObjectType1`
-    FOREIGN KEY (`gameObjectType_id`)
-    REFERENCES `bachelors`.`gameObjectType` (`id`)
+  PRIMARY KEY (`game_object_type_id`, `attribute_key_id`),
+  INDEX `fk_game_object_type_attribute_attribute_key1_idx` (`attribute_key_id` ASC),
+  CONSTRAINT `fk_game_object_type_attribute_game_object_type1`
+    FOREIGN KEY (`game_object_type_id`)
+    REFERENCES `bachelors`.`game_object_type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_gameObjectTypeAttribute_attributeKey1`
-    FOREIGN KEY (`attributeKey_id`)
-    REFERENCES `bachelors`.`attributeKey` (`id`)
+  CONSTRAINT `fk_game_object_type_attribute_attribute_key1`
+    FOREIGN KEY (`attribute_key_id`)
+    REFERENCES `bachelors`.`attribute_key` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `bachelors`.`userInventory`
+-- Table `bachelors`.`user_inventory`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bachelors`.`userInventory` ;
+DROP TABLE IF EXISTS `bachelors`.`user_inventory` ;
 
-CREATE TABLE IF NOT EXISTS `bachelors`.`userInventory` (
-  `user_id` INT UNSIGNED NOT NULL,
-  `item_id` INT UNSIGNED NOT NULL,
-  `count` VARCHAR(45) NULL,
-  PRIMARY KEY (`user_id`, `item_id`),
-  INDEX `fk_user_has_gameObjectType_gameObjectType1_idx` (`item_id` ASC),
-  INDEX `fk_user_has_gameObjectType_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_user_has_gameObjectType_user1`
+CREATE TABLE IF NOT EXISTS `bachelors`.`user_inventory` (
+  `user_id` VARCHAR(40) NOT NULL,
+  `game_object_type_id` INT UNSIGNED NOT NULL,
+  `count` INT NULL DEFAULT 1,
+  PRIMARY KEY (`user_id`, `game_object_type_id`),
+  INDEX `fk_userInventory_user1_idx` (`user_id` ASC),
+  INDEX `fk_user_inventory_game_object_type1_idx` (`game_object_type_id` ASC),
+  CONSTRAINT `fk_userInventory_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `bachelors`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_has_gameObjectType_gameObjectType1`
-    FOREIGN KEY (`item_id`)
-    REFERENCES `bachelors`.`gameObjectType` (`id`)
+  CONSTRAINT `fk_user_inventory_game_object_type1`
+    FOREIGN KEY (`game_object_type_id`)
+    REFERENCES `bachelors`.`game_object_type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `bachelors`.`game_object_content`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bachelors`.`game_object_content` ;
+
+CREATE TABLE IF NOT EXISTS `bachelors`.`game_object_content` (
+  `parent_id` INT UNSIGNED NOT NULL,
+  `child_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`parent_id`, `child_id`),
+  INDEX `fk_game_object_has_game_object_game_object2_idx` (`child_id` ASC),
+  INDEX `fk_game_object_has_game_object_game_object1_idx` (`parent_id` ASC),
+  CONSTRAINT `fk_game_object_has_game_object_game_object1`
+    FOREIGN KEY (`parent_id`)
+    REFERENCES `bachelors`.`game_object` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_game_object_has_game_object_game_object2`
+    FOREIGN KEY (`child_id`)
+    REFERENCES `bachelors`.`game_object` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
